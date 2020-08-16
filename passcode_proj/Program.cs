5127008b21e2afd873dc4317f05db4a969184e48
marco villanueva <villanuevamarco98@gmail.com>
2020-08-15 18:49:51 -0600

    notes on while loops

--- passcode_proj/Program.cs
+++ passcode_proj/Program.cs
@@ -7,7 +7,10 @@ namespace passcode_proj
         static void Main(string[] args)
         {
             var code = "";
-
+            //this is how the code perpetuates
+            //as long as the code isn't 'frites'
+            //keep prompting user to enter their
+            //secret word
             while (code != "frites")
             {
 
@@ -18,6 +21,7 @@ namespace passcode_proj
                 {
                     Console.WriteLine("You may enter...");
                 }
+                //this kicks the loop back to 'please enter the secret word!'
                 else if (code != "frites")
                 {
                     Console.WriteLine("Incorrect...try again");
