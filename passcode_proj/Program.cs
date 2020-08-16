327e99fe46d2d5bb1549ef29f4448ff29a4e5df2
marco villanueva <villanuevamarco98@gmail.com>
2020-08-15 20:08:07 -0600

    notes for while loop

--- passcode_proj/Program.cs
+++ passcode_proj/Program.cs
@@ -15,13 +15,17 @@ namespace passcode_proj
             {
 
                 Console.WriteLine("Please enter the secret word!");
+                //code starts as an empty string on line 9
+                //code on line 20 is what user enters in terminal
                 code = Console.ReadLine();
-
+                //when user enters 'frites', code breaks out of loop
                 if (code == "frites")
                 {
+                    //end of loop
                     Console.WriteLine("You may enter...");
                 }
                 //this kicks the loop back to 'please enter the secret word!'
+                //if user enters incorrect code on line 20
                 else if (code != "frites")
                 {
                     Console.WriteLine("Incorrect...try again");
